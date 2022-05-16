ca1e02f562ac5da95e87e68234b86062627d9bf8
DKDLP <s930049s@gmail.com>
2022-05-16 21:07:53 +0800

    v1.2

--- pwd_retry.py
+++ pwd_retry.py
@@ -1,21 +1,19 @@
 password = 'a123456'
-typing_num = 0
+typing_num = 3 #剩餘機會
 
 
-while True:
-    typing_num = typing_num + 1
-    if typing_num > 3:
-        print ("錯誤超過三次，自動關閉程式")
-        break
-
+while typing_num > 0:
+    typing_num = typing_num - 1
     typing_pwd = input("請輸入密碼: ")
-    
-    if typing_pwd != password:
-        if (3 - typing_num) > 0:
-            print ("密碼錯誤! 還有",str(3 - typing_num),"次機會")
-    
-    else:
+
+    if typing_pwd == password:
         print ("登入成功")
         break
+
+    else:
+        print ("密碼錯誤!")
+        if typing_num > 0:
+            print ("還剩下", typing_num, "次機會")
+
     
 
